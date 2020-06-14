6bcdf5eb9de0da9a2c8480842e00323e24ef62f6
he.zhu <he.zhu@thgoughtworks.com>
2020-06-15 00:13:21 +0800

    finish practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = function(items, sections){
+  let result = items;
+  Object.keys(sections).forEach(row =>{
+    let index = sections[row].index;
+    if(row>0){
+      index++;
+    }
+    result.splice(index, 0, sections[row].content);
+  })
+  return result;
+}
 export { inject };
